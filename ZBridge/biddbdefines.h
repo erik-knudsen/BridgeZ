c5c3a960b55b6c7e187c695bc4acf4c64d26ca25
Erik Aagaard Knudsen <eak.erik@gmail.com>
2016-12-10 17:25:25 +0100

    Working on automatic bidding.

--- ZBridge/biddbdefines.h
+++ ZBridge/biddbdefines.h
@@ -191,8 +191,8 @@ const int RR5_1S = 181;         //5 major. Open 1S.
 const int RR_3N_F = 182;        //3NT forcing raise.
 const int RR_2N_11_1821 = 183;  //2NT after 1/1: 18-21 HCP.
 const int RR_2N_11_1718 = 185;  //2NT after 1/1: 17-18 HCP.
-const int RR45_2N_21_1518 = 187;//2NT after 1/1: 18-21 HCP.
-const int RR45_2N_21_1214 = 190;//2NT after 1/1: 17-18 HCP.
+const int RR45_2N_21_1518 = 187;//2NT after 2/1: 15-18 HCP.
+const int RR45_2N_21_1214 = 190;//2NT after 2/1: 12-14 HCP.
 const int RR_4N = 191;          //4. suit natural.
 const int RR4_4N = 192;         //4 major. 4. suit natural.
 const int RR5_4N = 193;         //5 major. 4. suit natural.
@@ -206,11 +206,11 @@ const int RR_WN_1820_N = 200;   //Weak 1NT, 2NT after 1/1: 18-20, new minor natu
 const int RR_WN_1820_F = 201;   //Weak 1NT, 2NT after 1/1: 18-20, new minor forcing.
 const int RR_WN_1718_N = 202;   //Weak 1NT, 2NT after 1/1: 17-18, new minor natural.
 const int RR_WN_1718_F = 203;   //Weak 1NT, 2NT after 1/1: 17-18, new minor forcing.
-const int RR4_WN_1820_N = 204;  //4 major. Weak 1NT, 2NT after 1/1: 17-20, new minor natural.
+const int RR4_WN_1820_N = 204;  //4 major. Weak 1NT, 2NT after 1/1: 18-20, new minor natural.
 const int RR4_WN_1820_F = 205;  //4 major. Weak 1NT, 2NT after 1/1: 18-20, new minor forcing.
 const int RR4_WN_1718_N = 206;  //4 major. Weak 1NT, 2NT after 1/1: 17-18, new minor natural.
 const int RR4_WN_1718_F = 207;  //4 major. Weak 1NT, 2NT after 1/1: 17-18, new minor forcing.
-const int RR5_WN_1820_N = 208;  //5 major. Weak 1NT, 2NT after 1/1: 17-20, new minor natural.
+const int RR5_WN_1820_N = 208;  //5 major. Weak 1NT, 2NT after 1/1: 18-20, new minor natural.
 const int RR5_WN_1820_F = 209;  //5 major. Weak 1NT, 2NT after 1/1: 18-20, new minor forcing.
 const int RR5_WN_1718_N = 211;  //5 major. Weak 1NT, 2NT after 1/1: 17-18, new minor natural.
 const int RR5_WN_1718_F = 212;  //5 major. Weak 1NT, 2NT after 1/1: 17-18, new minor forcing.
@@ -425,5 +425,507 @@ const int D_RB_ACO = 421;       //After constructive overcall.
 const int D_RB_MO = 422;        //Rebid by opener and responder.
 const int D_RB_MO_OFF = 423;    //Maximal overcall double off.
 
+/*
+    Possible combinations ----------------------------------------------------------------
+
+    //Weak jump overcall (6-9 HCP)
+        //2NT Natural.
+        //2NT Ogust.
+        //2NT Feature.
+    //Intermediate jump overcall (13-16 HCP).
+
+    //Strong cuebid.
+    //Michaels cue bid.
+
+    //Penalty double against overcall.
+    //Negative double against overcall.
+
+    //Forcing raise (American).
+    //Simple Limit raise (European).
+        //Simple limit raise only.
+        //Simple limit raise + 2NT major raise forcing (Jacoby 2NT).
+        //Splinter.
+
+//1NT.
+    //Stayman forcing.
+    //Stayman not forcing.
+
+    //2D/2H/2S sign off.
+    //Jacoby.
+        //2S sign off.
+        //2S,2N minor transfer.
+
+    //3C/3D slam try.
+    //3C/3D game try.
+    //3C/3D pre emptive.
+
+    //3H/3S slam try.
+    //3H/3S game forcing.
+    //3H/3S pre emptive.
+    //3H/3S game invitation.
+
+    //4H/4S natural.
+
+//1NT 12-14 HCP.
+//1NT 16-18 HCP.
+//1NT 15-17 HCP.
+
+
+//2NT after 1/1 18-20 HCP.
+//2NT after 1/1 17-18 HCP.
+
+//2NT after 2/1 12-14 HCP
+//2NT after 2/1 15-18 HCP
+
+//After openers 1NT rebids.
+    //New minor natural.
+    //New minor forcing.
+
+//4-major.
+    //Strong 1NT, lower first.
+    //Weak 1NT, lower first.
+//5-major.
+    //Strong 1NT, lower first.
+    //Weak 1NT, lower first.
+
+//Strong 2C.
+    //Weak 2D, 6-11 HCP. //Weak 2H/2S, 6-11 HCP.
+
+    //Ogust after weak 2D, not forcing. //Ogust after weak 2H/2S, not forcing.
+    //Ogust after weak 2D, forcing. //Ogust after weak 2H/2S, forcing.
+    //Feature after weak 2D, not forcing. //Feature after weak 2H/2S, not forcing.
+    //Feature after weak 2D, forcing. //Feature after weak 2H/2S, forcing.
+//Strong two (2C, 2D, 2H, 2S).
+
+//2NT 20-21 HCP.
+//2NT 21-22 HCP.
+//2NT 22-24 HCP.
+
+//After 2C-2D-2NT opening. 22-24 HCP.
+//After 2C-2D-2NT opening. 23-24 HCP.
+
+//3NT 25-27 HCP.
+//3NT gambling (1., 2. and 3. seat). //3NT gambling (4. seat).
+//3NT Acol.
+
+
+//Defines for bid database -----------------------------------------------------------
+//Common.
+    OR_COM_1 OR_COM_2 RR_COM
+    OR_1m_2m_FL
+    D_C_COMMON D_D_COMMON D_H_COMMON D_S_COMMON
+    D_C1_916 D_D1_916 D_H1_916 D_S1_916
+    D_C_12 D_D_12 D_H_12 D_S_12
+    D_OPS_COMMON
+    D_OVS_COMMON D_OVS_RDP D_OVS_CONO D_OVS_RD_1 D_OVS_RD_2
+    D_RO_COMMON D_O_IND D_mLF
+    D_SNT_OFF
+    D_B_PO_COMMON
+    D_RB_COMMON_1 D_RB_SD_OFF D_RB_COMMON_2 D_RB_mLF D_RB_ACO
+    D_RB_MO D_RB_MO_OFF
+
+    //Weak jump overcall (6-9 HCP)
+        D_C2_1116_69 D_D2_1116_69 D_H2_1116_69 D_S2_1116_69
+        D_WJO_SNT_OFF
+        //2NT Natural.
+            D_OVS_R_WJO_N
+        //2NT Ogust.
+            D_OVS_R_WJO_O
+        //2NT Feature.
+            D_OVS_R_WJO_F
+    //Intermediate jump overcall (13-16 HCP)
+        D_C2_1116_1316 D_D2_1116_1316 D_H2_1116_1316 D_S2_1116_1316
+        D_OVS_R_IJO
+        D_IJO_SNT_OFF
+
+    //Strong cuebid.
+        D_C_SCB D_D_SCB D_H_SCB D_S_SCB
+        D_OPS_SCB
+        D_OVS_R_SCB
+        D_MCB_OFF
+    //Michaels cue bid.
+        D_C_MCB D_D_MCB D_H_MCB D_S_MCB
+        D_OPS_MCB
+        D_OVS_R_MCB
+        D_MCB_ON
+
+    //Penalty double against overcall.
+        D_OPS_PD
+    //Negative double against overcall.
+        D_OPS_ND
+        D_OVS_ND
+
+    //Forcing raise (American).
+        OR_1m_3m_F ORA_1C_1N ORA_1m_2N ORA_1m_3N OR_1M_3M_F
+        RR_4N
+    //Simple Limit raise (European).
+        OR_1m_3m_L ORE_1C_1N ORE_1m_2N ORE_1m_3N OR_1M_3M_L
+        RR_4F
+        D_OPS_L
+
+        //Simple limit raise only.
+            ORE45_1M_2N_N
+        //Simple limit raise + 2NT major raise forcing (Jacoby 2NT).
+            ORE45_1M_2N_J
+
+        //Splinter.
+            OR_SPLINTER RR_S
+
+    //Weak 1NT.
+        D_W
+        D_W_NO D_W_N2C_N D_W_N2D_N D_W_N2H2S_N
+        D_B_PO_W
+
+        //2D/2H/2S sign off.
+            D_W_SO
+        //Jacoby.
+            D_W_J
+            //2S sign off.
+            //2S,2N minor transfer.
+                D_W_J_MT
+
+        //3C/3D slam try or 3C/3D game try.
+            D_W_3mSGT
+
+        //3C/3D slam try.
+            D_W_J_MT
+
+        //3C/3D pre emptive and (3H/3S slam try or game forcing or game invitation).
+            D_W_3mPE3M
+
+        //3H/3S pre emptive.
+            D_W_3MPE
+
+        //4H/4S natural.
+            D_W_4MN
+
+        //2NT after 1/1 18-20 HCP.
+            //New minor natural (after 1NT rebid).
+                RR_WN_1820_N
+            //New minor forcing (after 1NT rebid).
+                RR_WN_1820_F
+        //2NT after 1/1 17-18 HCP.
+            //New minor natural (after 1NT rebid).
+                RR_WN_1718_N
+            //New minor forcing (after 1NT rebid).
+                RR_WN_1718_F
+
+    //Strong 1NT.
+        D_S
+        D_S_NO D_S_N2C_N D_S_N2D_N D_S_N2H2S_N
+        D_B_PO_S
+
+        //2D/2H/2S sign off.
+            D_S_SO
+        //Jacoby.
+            D_S_J
+            //2S sign off.
+            //2S,2N minor transfer.
+                D_S_J_MT
+
+        //3C/3D slam try or 3C/3D game try.
+            D_S_3mSGT
+
+        //3C/3D slam try.
+            D_S_J_MT
+
+        //3C/3D pre emptive and (3H/3S slam try or game forcing or game invitation).
+            D_S_3mPE3M
+
+        //3H/3S pre emptive.
+            D_S_3MPE
+
+        //4H/4S natural.
+            D_S_4MN
+
+        //New minor natural (after 1NT rebid).
+            RR_SN_N
+        //New minor forcing (after 1NT rebid).
+            RR_SN_F
+
+
+//4-major.
+    OR4_1H_1S OR4_1H_2 OR4_1S_2 OR4_1H OR4_1S OR4_1H_1N OR4_1S_1N OR4_1H_3N_N OR4_1S_3N_N
+    RR4_1D1H RR4_1S
+
+    //Penalty double against overcall.
+        D4_OPS_1H_PD D4_OPS_1S_PD
+    //Negative double against overcall.
+        D4_OPS_1H_ND D4_OPS_1S_ND
+
+    //Strong 1NT, lower first.
+        O12_4M_SN_L O3_4M_SN_L O4_4M_SN_L
+        //New minor natural (after 1NT rebid).
+            RR4_SN_N
+        //New minor forcing (after 1NT rebid).
+            RR4_SN_F
+    //Weak 1NT, lower first.
+        O12_4M_WN_L O3_4M_WN_L O4_4M_WN_L
+        //2NT after 1/1 18-20 HCP.
+            //New minor natural (after 1NT rebid).
+                RR4_WN_1820_N
+            //New minor forcing (after 1NT rebid).
+                RR4_WN_1820_F
+        //2NT after 1/1 17-18 HCP.
+            //New minor natural (after 1NT rebid).
+                RR4_WN_1718_N
+            //New minor forcing (after 1NT rebid).
+                RR4_WN_1718_F
+
+    //Forcing raise (American).
+        A4_FF ORA4_1M_2N_N ORA4_1M_3N_N
+        RR4_4N
+    //Simple limit raise (European).
+        RR4_4F_1D1H RR4_4F_1S
+        //Non Splinter.
+            E45_SL E4_1H_SL E4_1S_SL
+        //Splinter.
+            E45_L_S
+
+                //Simple limit raise only.
+                    E4_1H_L_S E4_1S_L_S
+                //Simple limit raise + 2NT major raise forcing (Jacoby 2NT).
+                    AE4_1H_2NF_S AE4_1S_2NF_S
+
+//5-major.
+    OR5_1H_1S OR5_1H_2 OR5_1S_2 OR5_1H OR5_1S OR5_1H_1N OR5_1H_1N OR5_1H_3N_N OR5_1S_3N_N
+    RR5_1D1H RR5_1S
+
+    //Penalty double against overcall.
+        D5_OPS_1H_PD D5_OPS_1S_PD
+    //Negative double against overcall.
+        D5_OPS_1H_ND D5_OPS_1S_ND
+
+    //Strong 1NT.
+        O12_5M_SN O3_5M_SN O4_5M_SN
+        //New minor natural (after 1NT rebid).
+            RR5_SN_N
+        //New minor forcing (after 1NT rebid).
+            RR5_SN_F
+    //Weak 1NT.
+        O12_5M_WN O3_5M_WN O4_5M_WN
+        //2NT after 1/1 18-20 HCP.
+            //New minor natural (after 1NT rebid).
+                RR5_WN_1820_N
+            //New minor forcing (after 1NT rebid).
+                RR5_WN_1820_F
+        //2NT after 1/1 17-18 HCP.
+            //New minor natural (after 1NT rebid).
+                RR5_WN_1718_N
+            //New minor forcing (after 1NT rebid).
+                RR5_WN_1718_F
+
+    //Forcing raise (American).
+        A5_FF ORA5_1M_2N_N ORA5_1M_3N_N
+        RR5_4N
+    //Simple limit raise (European).
+        RR5_4F_1D1H RR5_4F_1S
+        //Non Splinter.
+            E45_SL E5_1H_SL E5_1S_SL
+        //Splinter.
+            E45_L_S
+
+                //Simple limit raise only.
+                    E5_1H_L_S E5_1S_L_S
+                //Simple limit raise + 2NT major raise forcing (Jacoby 2NT).
+                    AE5_1H_2NF_S AE5_1S_2NF_S
+
+//1NT 12-14 HCP.
+    O12_1N_12_14 O3_1N_12_14 O4_1N_12_14 R_1214 OR_1214_4MN
+    //Stayman forcing.
+        R_1214_SF OR_1214_SF_1 OR_1214_SF_2
+    //Stayman not forcing.
+        R_1214_SN OR_1214_SN_1 OR_1214_SN_2
+
+    //2D/2H/2S sign off.
+        R_1214_SO OR_1214_SO
+    //Jacoby.
+        OR_1214_J
+        //2S sign off.
+            R_1214_J_SO OR_1214_J_SO
+        //2S,2N minor transfer.
+            R_1214_J_MT OR_1214_J_MT
+
+    //3C/3D slam try.
+        R_1214_3mST OR_1214_3mST
+    //3C/3D game try.
+        R_1214_3mGT OR_1214_3mGT
+    //3C/3D pre emptive.
+        R_1214_3mPE OR_1214_3mPE
+
+    //3H/3S slam try.
+        R_1214_3MST OR_1214_3MST
+    //3H/3S game forcing.
+        R_1214_3MGF OR_1214_3MGF
+    //3H/3S pre emptive.
+        R_1214_3MGPE OR_1214_3MPE
+    //3H/3S game invitation.
+        R_1214_3MGI OR_1214_3MGI
+
+    //4H/4S natural.
+        R_1214_4MN
+//1NT 16-18 HCP.
+    O_1N_16_18 R_1618 OR_1618_4MN
+    //Stayman forcing.
+        R_1618_SF OR_1618_SF_1 OR_1618_SF_2
+    //Stayman not forcing.
+        R_1618_SN OR_1618_SN_1 OR_1618_SN_2
+
+    //2D/2H/2S sign off.
+        R_1618_SO OR_1618_SO
+    //Jacoby.
+        OR_1618_J
+        //2S sign off.
+            R_1618_J_SO OR_1618_J_SO
+        //2S,2N minor transfer.
+            R_1618_J_MT OR_1618_J_MT
+
+    //3C/3D slam try.
+        R_1618_3mST OR_1618_3mST
+    //3C/3D game try.
+        R_1618_3mGT OR_1618_3mGT
+    //3C/3D pre emptive.
+        R_1618_3mPE OR_1618_3mPE
+
+    //3H/3S slam try.
+        R_1618_3MST OR_1618_3MST
+    //3H/3S game forcing
+        R_1618_3MGF OR_1618_3MGF
+    //3H/3S pre emptive.
+        R_1618_3MGPE OR_1618_3MPE
+    //3H/3S game invitation.
+        R_1618_3MGI OR_1618_3MGI
+
+    //4H/4S natural.
+        R_1618_4MN
+//15-17 HCP.
+    O_1N_15_17 R_1517 OR_1517_4MN
+    //Stayman forcing.
+        R_1517_SF OR_1517_SF_1 OR_1517_SF_2
+    //Stayman not forcing.
+        R_1517_SN OR_1517_SN_1 OR_1517_SN_2
+
+    //2D/2H/2S sign off.
+        R_1517_SO OR_1517_SO
+    //Jacoby.
+        OR_1517_J
+        //2S sign off.
+            R_1517_J_SO OR_1517_J_SO
+        //2S,2N minor transfer.
+            R_1517_J_MT OR_1517_J_MT
+
+    //3C/3D slam try.
+        R_1517_3mST OR_1517_3mST
+    //3C/3D game try.
+        R_1517_3mGT OR_1517_3mGT
+    //3C/3D pre emptive.
+        R_1517_3mPE OR_1517_3mPE
+
+    //3H/3S slam try.
+        R_1517_3MST OR_1517_3MST
+    //3H/3S game forcing
+        R_1517_3MGF OR_1517_3MGF
+    //3H/3S pre emptive.
+        R_1517_3MGPE OR_1517_3MPE
+    //3H/3S game invitation.
+        R_1517_3MGI OR_1517_3MGI
+
+    //4H/4S natural.
+        R_1517_4MN
+
+//Strong 2C.
+    O_S2_C A_S2_C
+    //Weak 2D, 6-11 HCP. //Weak 2H/2S, 6-11 HCP.
+    O12_W2D_6_11 O3_W2D_6_11 O4_W2D_6_11
+    O12_W2H2S_6_11 O3_W2H2S_6_11 O4_W2H2S_6_11
+    A_W2D A_W2H2S A_W2DX_NAT A_W2H2SX_NAT
+
+    //Ogust after weak 2D, not forcing. //Ogust after weak 2H/2S, not forcing.
+        A_W2D_O_NF A_W2H2S_O_NF
+    //Ogust after weak 2D, forcing. //Ogust after weak 2H/2S, forcing.
+        A_W2D_O_F A_W2H2S_O_F
+    //Feature after weak 2D, not forcing. //Feature after weak 2H/2S, not forcing.
+        A_W2D_F_NF A_W2H2S_F_NF
+    //Feature after weak 2D, forcing. //Feature after weak 2H/2S, forcing.
+        A_W2D_F_F A_W2H2S_F_F
+//Strong two (2C, 2D, 2H, 2S).
+    O_S2_CDHS A_S2_CDHS
+
+//2NT 20-21 HCP.
+    O_2N_20_21 A_2N_20_21 A_2N_20_21_L
+    //Non Texas.
+    A_2N_20_21_NT
+    //3C Stayman. 3D, 3H, 3S Natural.
+    A_2N_20_21_SN
+    //3C Stayman. Later bids.
+    A_2N_20_21_SL
+    //3D, 3H, 3S Natural. Later bids.
+    A_2N_20_21_NL
+//2NT 21-22 HCP.
+    O_2N_21_22 A_2N_21_22 A_2N_21_22_L
+    //Non Texas.
+    A_2N_21_22_NT
+    //3C Stayman. 3D, 3H, 3S Natural.
+    A_2N_21_22_SN
+    //3C Stayman. Later bids.
+    A_2N_21_22_SL
+    //3D, 3H, 3S Natural. Later bids.
+    A_2N_21_22_NL
+//2NT 22-24 HCP.
+    O_2N_22_24 A_2N_22_24 A_2N_22_24_L
+    //Non Texas.
+    A_2N_22_24_NT
+    //3C Stayman. 3D, 3H, 3S Natural.
+    A_2N_22_24_SN
+    //3C Stayman. Later bids.
+    A_2N_22_24_SL
+    //3D, 3H, 3S Natural. Later bids.
+    A_2N_22_24_NL
+
+//2NT after 1/1 18-21 HCP
+    RR_2N_11_1821
+//2NT after 1/1 17-18 HCP
+    RR_2N_11_1718
+
+//2NT after 2/1 15-18 HCP
+    RR45_2N_21_1518
+//2NT after 2/1 15-18 HCP
+    RR45_2N_21_1214
+
+//After 2C-2D-2NT opening.
+//22-24 HCP.
+    A_2C2D2N_22_24
+    //Non Texas.
+    A_2C2D2N_22_24_NT
+    //3C Stayman. 3D, 3H, 3S Natural.
+    A_2C2D2N_22_24_SN
+    //3C Stayman. Later bids.
+    A_2C2D2N_22_24_SL
+    //3D, 3H, 3S Natural. Later bids.
+    A_2C2D2N_22_24_NL
+//After 2C-2D-2NT opening.
+//23-24 HCP.
+    A_2C2D2N_23_24
+    //Non Texas.
+    A_2C2D2N_23_24_NT
+    //3C Stayman. 3D, 3H, 3S Natural.
+    A_2C2D2N_23_24_SN
+    //3C Stayman. Later bids.
+    A_2C2D2N_23_24_SL
+    //3D, 3H, 3S Natural. Later bids.
+    A_2C2D2N_23_24_NL
+
+//3. level.
+    O12_3LEVEL O3_3LEVEL O4_3LEVEL A_3LEVEL
+
+//3NT 25-27 HCP.
+    O_3N_25_27 A_3N_25_27
+//3NT gambling (1., 2. and 3. seat). //3NT gambling (4. seat).
+    O123_3N_GMBL A_3N_GMBL
+    O4_3N_GMBL
+//3NT Acol.
+    O_3N_ACOL A_3N_ACOL
+
+*/
 #endif // BIDDBDEFINES
 
